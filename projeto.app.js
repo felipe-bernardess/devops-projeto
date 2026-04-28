75e3fbdbee5e0f8b04b50b56aabdce102ee43c90
felipe-bernardess <felptibernardes@gmail.com>
2026-04-27 22:03:01 -0300

    Refatora projeto.app.js

--- projeto.app.js
+++ projeto.app.js
@@ -1,6 +1,3 @@
-Teste-Alerta-novo
-//teste
-main
 const http = require('http');
 
 const listaTarefas = [
@@ -9,20 +6,24 @@ const listaTarefas = [
   'Aprender Docker'
 ];
 
-const servidor = http.createServer((req, res) => {
-
+function handleRequest(req, res) {
   if (req.url === '/') {
     res.write('Teste de rota principal funcionando');
   } 
   else if (req.url === '/tarefas') {
-    res.setHeader('Content-Type', 'application/json'); // 👈 novo
+    res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify(listaTarefas));
   } 
   else {
     res.write('Rota não encontrada');
   }
-
   res.end();
-});
+}
 
-servidor.listen(3000);
+const servidor = http.createServer(handleRequest);
+
+if (require.main === module) {
+  servidor.listen(3000);
+}
+
+module.exports = { handleRequest, listaTarefas };
